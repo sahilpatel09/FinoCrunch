408df72523b921e992f569f44abc29e8804ccffb
Sahil <smppatel999@gmail.com>
2022-03-05 16:34:01 +0530

    Add Spinner in the Dashboard.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="relative antialiased bg-gray-800">
+  <div v-if="loading" class="relative antialiased bg-gray-800">
     <navbar />
     <router-view />
   </div>
+
+  <Spinner v-else/>
 </template>
 <script>
 // import MainScreen from '../components/MainScreen.vue';
 import navbar from "../components/Dashboard/AdminNavbar.vue";
+import Spinner from "@/components/Spinner";
 export default {
   name: "Home",
+  data() {
+    return{
+      loading: false,
+    }
+  },
   components: {
+    Spinner,
     // sidebar,
     navbar,
   },

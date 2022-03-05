7e9d0d4202f97d03583afa2ef141fb4834ac1492
Sahil <smppatel999@gmail.com>
2022-03-05 16:32:55 +0530

    Spinner Component.

---
+++ src/components/Spinner.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex items-center justify-center bg-gray-900 w-screen h-screen">
+    <div class="w-40 h-40 border-t-4 border-b-4 border-gray-100 rounded-full animate-spin">
+    </div>
+    <span class="text-white text-2xl" style="font-family: Brush Script MT, Brush Script Std, cursive	; margin-left: -128px;">FinoCrunch</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Spinner"
+}
+</script>
+
+<style scoped>
+
+</style>

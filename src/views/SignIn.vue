1fa1e3918385fb6e3622978f3b5eb600ea3cb473
Sahil <smppatel999@gmail.com>
2022-03-05 12:15:56 +0530

    Fix css.

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-row items-center justify-center bg-gray-800 text-gray-300 h-screen">
+  <div class="flex flex-row items-center justify-center bg-gray-800 text-gray-300 h-full">
     <div class="">
       <div
         class="flex flex-col rounded items-center justify-center px-16 py-14 sm:shadow-2xl bg-gray-900"
@@ -7,7 +7,9 @@
         <img src="../assets/logo-dark.png" alt="" class="w-32 mt-4" />
         <h2 class="font-serif text-4xl antialiased font-bold">FinoCrunch</h2>
         <p class="text-md text-slate-50">Record Everything important.</p>
-
+        <div class="w-24 h-0.5 bg-indigo-100 mt-2"></div>
+        <h4 class="font-semibold my-1">SIGN IN</h4>
+        <div class="w-24 h-0.5 bg-indigo-100"></div>
         <button
           class="mt-4 border-2 border-sky-500 px-8 py-2 flex flex-row items-center gap-2 rounded w-64 md:hover:bg-gray-100 md:ease-in md:duration-300"
         >

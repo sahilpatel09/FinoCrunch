db18c5015423b597f75b12fb7c15cc64ac62d1a1
Sahil <smppatel999@gmail.com>
2022-03-07 20:41:49 +0530

    CSS for fade-in-left & fade-in-down

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <template>
-  <div v-if="loading" class="relative antialiased bg-gray-800">
-    <navbar :name="user.user_metadata.name"/>
+  <div v-if="loading" class="relative antialiased bg-gray-800 in-down">
+    <navbar name="user"/>
     <router-view />
   </div>
   <Spinner v-else/>
@@ -8,18 +8,24 @@
 <script>
 // import MainScreen from '../components/MainScreen.vue';
 import navbar from "../components/Dashboard/AdminNavbar.vue";
-import Spinner from "@/components/Spinner";
+import Spinner from "@/components/Loader";
 export default {
   name: "Home",
   data() {
     return{
-      loading: false,
+      loading: true,
       user: this.$store.currentUser
     }
-  },created() {
+  },
+  beforeCreate() {
+    console.log(this.$store.currentUser)
     if(this.$store.currentUser){
       this.loading = true
     }
+  },
+  created() {
+
+
   },
   components: {
     Spinner,
@@ -28,3 +34,7 @@ export default {
   },
 };
 </script>
+<style>
+
+
+</style>
